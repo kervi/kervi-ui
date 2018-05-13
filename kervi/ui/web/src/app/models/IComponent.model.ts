2d1e061d40ed1de0b2051f503b13ec2e7e895244
Tim Wentzlau <tim@wentzlau.dk>
2018-05-13 22:07:07 +0100

    Add support for DashboardLink

--- kervi/ui/web/src/app/models/IComponent.model.ts
+++ kervi/ui/web/src/app/models/IComponent.model.ts
@@ -1,11 +1,34 @@
 import { KerviService } from '../kervi.service'
+
+export class DashboardLink{
+    dashboardId:string;
+    sectionId:string;
+    sectionName:string;
+    component:IComponent;
+    parameters:any;
+
+    /*constructor(dashboardId:string, sectionId:string, sectionName:string){
+        this.dashboardId = dashboardId;
+        this.sectionId = sectionId;
+        this.sectionName = sectionName;
+    }*/
+
+    constructor(component:IComponent, message:any){
+        this.dashboardId = message.dashboardId;
+        this.sectionId = message.sectionId;
+        this.sectionName = message.sectionName;
+        this.component = component;
+        this.parameters = message.parameters;
+    }
+}
+
 export interface IComponent {
     id: string;
     name: string;
     visible: boolean;
     componentType: string;
     ui:any;
-    dashboards : string[];
+    dashboards : DashboardLink[];
 
     updateReferences(kerviService:KerviService);
     reload(component:IComponent);
